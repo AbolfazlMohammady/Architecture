4970205609e0f43c12a3310fe1e16777fd505306
Abolfazl Mohammady <abolfazlking22@gmail.com>
2025-06-14 11:39:32 +0330

    navbar

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('login/', views.LoginView.as_view(),name="login"),
     path('logout/', views.LogoutView.as_view(),name="logout"),
-    path('profile', views.ProfileView.as_view(),name="profile"),
+    path('profile/', views.ProfileView.as_view(),name="profile"),
     path('experiment/', include('experiment.urls')),
 ]
