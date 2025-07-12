b67d6176bcb2bf86387c51626da26fd59e1b947f
Abolfazl Mohammady <abolfazlking22@gmail.com>
2025-07-12 10:35:09 +0330

    add

--- project/templatetags/custom_filters.py
+++ project/templatetags/custom_filters.py
@@ -11,3 +11,17 @@ def persian_number(value):
     """Convert English digits in a string or int to Persian digits."""
     persian_digits = '۰۱۲۳۴۵۶۷۸۹'
     return ''.join(persian_digits[int(ch)] if ch.isdigit() else ch for ch in str(value))
+
+@register.filter
+def intcomma_float(value, decimal_places=3):
+    try:
+        value = float(value)
+    except (ValueError, TypeError):
+        return value
+    int_part, dot, frac_part = f"{value:.{decimal_places}f}".partition('.')
+    int_part = "{:,}".format(int(int_part))
+    frac_part = frac_part.rstrip('0')
+    if frac_part:
+        return f"{int_part}.{frac_part}"
+    else:
+        return int_part
