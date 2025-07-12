676f82796d7c4743f3829c2bb4111c4fadfdc041
Abolfazl Mohammady <abolfazlking22@gmail.com>
2025-07-12 14:55:35 +0330

    add

--- Online_monitoring_of_quality_control/settings.py
+++ Online_monitoring_of_quality_control/settings.py
@@ -118,13 +118,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'fa-ir'
+LANGUAGE_CODE = 'fa'
 
 TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
 
 
 # Static files (CSS, JavaScript, Images)
